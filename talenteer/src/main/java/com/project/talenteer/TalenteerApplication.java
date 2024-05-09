f7116474fe5f10d17c295ac7055f13a6e85a72b1
Yusuf Talha Kaya <yyusuftalhakaya@gmail.com>
2024-05-09 13:10:34 +0300

    Proje bağımlılıkları eklenerek "Spring Boot Projesi" oluşturuldu.

---
+++ talenteer/src/main/java/com/project/talenteer/TalenteerApplication.java
@@ -0,0 +1,13 @@
+package com.project.talenteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalenteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalenteerApplication.class, args);
+	}
+
+}
---
+++ talenteer/src/test/java/com/project/talenteer/TalenteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.talenteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalenteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
