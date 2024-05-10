779d546eea44cfc211e27876f5ec4006d3198273
Yusuf Talha Kaya <yyusuftalhakaya@gmail.com>
2024-05-10 18:20:33 +0300

    Update EducationServiceImpl.java

--- talenteer/src/main/java/com/project/talenteer/service/impl/EducationServiceImpl.java
+++ talenteer/src/main/java/com/project/talenteer/service/impl/EducationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.project.talenteer.service.impl;
 
+import com.project.talenteer.core.entity.User;
+import com.project.talenteer.core.repository.UserRepository;
 import com.project.talenteer.core.utilities.result.DataResult;
 import com.project.talenteer.core.utilities.result.SuccessDataResult;
 import com.project.talenteer.model.entity.Education;
@@ -13,16 +15,22 @@ import org.springframework.stereotype.Service;
 public class EducationServiceImpl implements EducationService {
 
     private final EducationRepository educationRepository;
+    private final UserRepository userRepository; 
 
     @Autowired
-    public EducationServiceImpl(EducationRepository educationRepository) {
+    public EducationServiceImpl(EducationRepository educationRepository,UserRepository userRepository) {
         this.educationRepository = educationRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public DataResult<Education> saveEducation(Education education) {
-        return new SuccessDataResult<>(educationRepository.save(education), "Education saved successfully.");
+    User user = userRepository.findById(education.getUser().getId())
+            .orElseThrow(() -> new RuntimeException("User not found with id: " + education.getUser().getId()));
+    education.setUser(user);
+    return new SuccessDataResult<>(educationRepository.save(education), "Education saved successfully.");
     }
+
 
     @Override
     public DataResult<Education> getEducationByUserId(int userId) {
