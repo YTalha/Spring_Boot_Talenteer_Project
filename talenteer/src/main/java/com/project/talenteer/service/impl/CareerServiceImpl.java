92265f7b9c8de60af6203f1dc6152c027a534a40
Yusuf Talha Kaya <yyusuftalhakaya@gmail.com>
2024-05-10 18:12:40 +0300

    Update CareerServiceImpl.java

--- talenteer/src/main/java/com/project/talenteer/service/impl/CareerServiceImpl.java
+++ talenteer/src/main/java/com/project/talenteer/service/impl/CareerServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.project.talenteer.core.utilities.result.DataResult;
 import com.project.talenteer.model.entity.Career;
 import com.project.talenteer.repository.CareerRepository;
+import com.project.talenteer.core.entity.User;
+import com.project.talenteer.core.repository.UserRepository;
 import com.project.talenteer.service.CareerService;
 import com.project.talenteer.core.utilities.result.SuccessDataResult;
 
@@ -13,14 +15,19 @@ import com.project.talenteer.core.utilities.result.SuccessDataResult;
 public class CareerServiceImpl implements CareerService{
 
     private final CareerRepository careerRepository;
+    private final UserRepository userRepository; 
 
     @Autowired
-    public CareerServiceImpl(CareerRepository careerRepository) {
+    public CareerServiceImpl(CareerRepository careerRepository, UserRepository userRepository) {
         this.careerRepository = careerRepository;
+        this.userRepository = userRepository; 
     }
 
     @Override
     public DataResult<Career> saveCareer(Career career) {
+        User user = userRepository.findById(career.getUser().getId())
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + career.getUser().getId()));
+        career.setUser(user);
         return new SuccessDataResult<>(careerRepository.save(career), "Career saved successfully.");
     }
 
